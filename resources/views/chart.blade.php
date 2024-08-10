050275ff2a81c2d297249ccc0d90e5111c31c2f5
akmmp241 <akmalmp241@gmail.com>
2024-08-10 16:08:26 +0700

    fix: type on resources

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chart OSIS</title>
     @vite('resources/css/app.css')
-    @vite('resource/js/app.js')
+    @vite('resources/js/app.js')
 </head>
 <script>
     const initData = {
