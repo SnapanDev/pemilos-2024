71451ca07822e74f2e022445a434d0e6e672841b
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2024-08-10 16:20:01 +0700

    Development (#6)
    * fix: type on resources
    
    * fix: update vite config for build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/*', 'resources/js/*', 'resources/js/main.ts'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/main.ts', 'resources/js/chart.js'],
             refresh: true,
         }),
     ],
